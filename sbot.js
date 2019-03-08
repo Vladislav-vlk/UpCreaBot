5c0eebf3ea6013fdf21decaff4889007d906ec38
ELTR11 <45146648+ELTR11@users.noreply.github.com>
2019-03-08 10:53:31 +0200

    Update sbot.js

--- sbot.js
+++ sbot.js
@@ -153,5 +153,5 @@ function getWeather(id, before, after){
 }
 function music(msg){
 	let randMusic = Math.floor(0 + Math.random() * 18);
-	bot.sendVoice(musicid[randMusic], {chat_id: msg.chat.id, caption: 'Вот, послушай');
+	bot.sendVoice(musicid[randMusic], {chat_id: msg.chat.id, caption: 'Вот, послушай'});
 }
